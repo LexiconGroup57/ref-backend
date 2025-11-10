94113616e3a76b9f41649e478d0892aa514bc512
tobiasengberg <tobias.engberg@exilic.se>
2025-11-10 22:45:10 +0100

    Add reference model

--- ref-backend/Program.cs
+++ ref-backend/Program.cs
@@ -1,9 +1,20 @@
+using ref_backend.models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("RefPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -14,31 +25,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
-
-var summaries = new[]
+app.UseCors("RefPolicy");
+app.MapGet("api/references", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+    return new List<RefRecord>() {new RefRecord(1, "The Great Gatsby", "Fitzgerald, Scott", "1925", "HarperCollins")};
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ ref-backend/models/RefRecord.cs
@@ -0,0 +1,21 @@
+namespace ref_backend.models;
+
+public class RefRecord
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Creator { get; set; }
+    public string Date { get; set; }
+    public string Publisher { get; set; }
+
+    public RefRecord() { }
+
+    public RefRecord(int id, string title, string creator, string date, string publisher)
+    {
+        Id = id;
+        Title = title;
+        Creator = creator;
+        Date = date;
+        Publisher = publisher;
+    }
+}
