0fd4b75d3578602967612247de61d9a0a37fdd7b
tobiasengberg <tobias.engberg@exilic.se>
2025-11-25 10:14:33 +0100

    Cookie and authorization policy

--- ref-backend/Program.cs
+++ ref-backend/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -6,20 +7,31 @@ using ref_backend.models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ReferenceDB>();
+builder.Services.ConfigureApplicationCookie(options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(2);
+        options.SlidingExpiration = true;
+    }
+);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ReferenceDB>(options =>
     options.UseSqlite("DataSource=references.db"));
 builder.Services.AddAuthorization();
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-    .AddEntityFrameworkStores<ReferenceDB>();
+// builder.Services.AddAuthorization(options =>
+// {
+//     options.FallbackPolicy = new AuthorizationPolicyBuilder()
+//         .RequireAuthenticatedUser()
+//         .Build();
+// });
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("RefPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:5178")
+        policy.WithOrigins("http://localhost:5175", "http://localhost:5173", "http://localhost:5174")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
@@ -42,20 +54,24 @@ using (var scope = app.Services.CreateScope())
 
 app.UseHttpsRedirection();
 app.UseCors("RefPolicy");
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapGroup("/user").MapIdentityApi<IdentityUser>();
 
 app.MapPost("/user/logout", async (SignInManager<IdentityUser> signInManager,
-    [FromBody] object empty) =>
-{
-    if (empty != null)
+        [FromBody] object empty) =>
     {
-        await signInManager.SignOutAsync();
-        return Results.Ok();
-    }
-    return Results.Unauthorized();
-})
-.WithOpenApi()
-.RequireAuthorization();
+        if (empty != null)
+        {
+            await signInManager.SignOutAsync();
+            return Results.Ok();
+        }
+
+        return Results.Unauthorized();
+    })
+    .RequireAuthorization()
+    .WithOpenApi();
 
 app.MapGet("api/references", (ReferenceDB _context) =>
     {
